4ce23b58e3cbac86be4309ea7b81bc719e374585
yumerov <yumerov.levent@gmail.com>
2025-05-14 22:30:26 +0300

    [kyu7][dotnet] fizz buzz

---
+++ DotNet/Kyu7/FizzBuzz.cs
@@ -0,0 +1,18 @@
+namespace DotNet.Kyu7;
+
+using System.Linq;
+
+public class FizzBuzz
+{
+    public static string Label(int number) => number switch
+    {
+        _ when number % 15 == 0 => "FizzBuzz",
+        _ when number % 3 == 0 => "Fizz",
+        _ when number % 5 == 0 => "Buzz",
+        _ => number.ToString()
+    };
+    
+    public static string[] GetFizzBuzzArray(int n) => Enumerable.Range(1, n).Select(Label).ToArray();
+
+    public static void Main() => Console.WriteLine(string.Join(", ", GetFizzBuzzArray(15)));
+}
