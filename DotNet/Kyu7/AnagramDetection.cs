06c36ceab0b9e41e428c5677f13025fffe86d93a
yumerov <yumerov.levent@gmail.com>
2025-05-14 22:03:04 +0300

    [kyu7][dotnet] anagram

---
+++ DotNet/Kyu7/AnagramDetection.cs
@@ -0,0 +1,12 @@
+namespace DotNet.Kyu7;
+
+using System.Linq;
+
+public class AnagramDetection
+{
+    private static string Normalize(string s) => string.Join("", s.ToLower().ToCharArray().Order());
+    
+    public static bool IsAnagram(string a, string b) => Normalize(a).Equals(Normalize(b));
+
+    public static void Main() => Console.WriteLine(IsAnagram("Buckethead", "DeathCubeK"));
+}
