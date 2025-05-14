68e73d9221c2474ac67ad81a001faa36d2ec9c88
yumerov <yumerov.levent@gmail.com>
2025-05-14 22:53:32 +0300

    [kyu7][dotnet] mumbling

---
+++ DotNet/Kyu7/Mumbling.cs
@@ -0,0 +1,13 @@
+namespace DotNet.Kyu7;
+
+public class Mumbling
+{
+    public static String Accum(string s) => string.Join("-", s.ToLower().ToCharArray().Select((c, i) => c.ToString().ToUpper() + "".PadLeft(i, c)));
+
+    public static void Main()
+    {
+        Console.WriteLine(Accum("abcd")); // "A-Bb-Ccc-Dddd"
+        Console.WriteLine(Accum("RqaEzty")); // "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+
+    }
+}
