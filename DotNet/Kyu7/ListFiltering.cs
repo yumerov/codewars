c23e5896956f339a82731eeeb78863ffa00f95f9
yumerov <yumerov.levent@gmail.com>
2025-05-17 20:36:43 +0300

    [kyu7][dotnet] List Filtering

---
+++ DotNet/Kyu7/ListFiltering.cs
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/53dbd5315a3c69eed20002dd/train/csharp
+
+namespace DotNet.Kyu7;
+
+public class ListFiltering
+{
+    public static IEnumerable<int> GetIntegersFromList(List<object> listOfItems) => listOfItems.OfType<int>();
+
+    public static void Main()
+    {
+        Console.WriteLine(string.Join(",", GetIntegersFromList([1, 2, "a", "b"])));
+    }
+}
