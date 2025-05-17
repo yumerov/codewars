4e0f50afbd2879dd2913662d4a6083dc888e3c01
yumerov <yumerov.levent@gmail.com>
2025-05-17 12:54:47 +0300

    [kyu8][dotnet] Well of Ideas - Easy Version

---
+++ DotNet/Kyu8/WellOfIdeasEasyVersion.cs
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/57f222ce69e09c3630000212/train/csharp
+
+namespace DotNet.Kyu8;
+
+public class WellOfIdeasEasyVersion
+{
+    public static string Well(string[] ideas) => ideas.Count(idea => idea.Equals("good")) switch
+    {
+        0 => "Fail!",
+        < 3 => "Publish!",
+        _ => "I smell a series!"
+    };
+
+    public static void Main()
+    {
+        Console.WriteLine(Well(["bad", "bad", "bad"])); // Fail!
+        Console.WriteLine(Well(["good", "bad", "bad", "bad", "bad"])); // Publish!
+        Console.WriteLine(Well([
+            "good", "bad", "bad", "bad", "bad", "good", "bad", "bad", "good"
+        ])); // I smell a series!
+    }
+}
