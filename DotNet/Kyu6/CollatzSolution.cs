7c357be51375352dca2179604e7c91a3990ca34f
yumerov <yumerov.levent@gmail.com>
2025-05-20 11:34:28 +0300

    [kyu6][dotnet] Collatz string

---
+++ DotNet/Kyu6/CollatzSolution.cs
@@ -0,0 +1,24 @@
+// https://www.codewars.com/kata/collatz/train/csharp
+
+namespace DotNet.Kyu6;
+
+
+public class CollatzSolution
+{
+    public static string Collatz(int n)
+    {
+        string output = "";
+        while (n != 1)
+        {
+            output += $"{n}->";
+            n = n % 2 == 0 ? n / 2 : n * 3 + 1;
+        }
+        
+        return $"{output}1";
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine(Collatz(3));
+    }
+}
