d4cc315a0fec5f0069ca023d0af54f301688cb2b
yumerov <yumerov.levent@gmail.com>
2025-05-21 16:31:22 +0300

    [kyu5][dotnet] Number of trailing zeros of N!

---
+++ DotNet/Kyu5/NumberOfTrailingZerosOfNFactorial.cs
@@ -0,0 +1,27 @@
+// https://www.codewars.com/kata/number-of-trailing-zeros-of-n/train/csharp
+
+namespace DotNet;
+
+public class NumberOfTrailingZerosOfNFactorial
+{
+    public static int TrailingZeros(int n)
+    {
+        if (n <= 1) return 0;
+
+        int sum = 0;
+        for (int index = (int)Math.Log(n, 5); index > 0; index--)
+        {
+            sum += n / (int)Math.Pow(5, index);
+        }
+
+        return sum;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine($"{TrailingZeros(5)} == 1");
+        Console.WriteLine($"{TrailingZeros(12)} == 2");
+        Console.WriteLine($"{TrailingZeros(25)} == 6");
+        Console.WriteLine($"{TrailingZeros(531)} == 131");
+    }
+}
