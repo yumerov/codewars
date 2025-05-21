15a01fd6eff86b548ca1a2bf28fab55a84f39c92
yumerov <yumerov.levent@gmail.com>
2025-05-21 13:52:39 +0300

    [kyu5][dotnet] First non-repeating character

---
+++ DotNet/Kyu5/FirstNonRepeatingCharacter.cs
@@ -0,0 +1,28 @@
+// https://www.codewars.com/kata/first-non-repeating-character/train/csharp
+
+namespace DotNet;
+
+using System.Linq;
+
+public class FirstNonRepeatingCharacter
+{
+    public static string FirstNonRepeatingLetter(string str)
+    {
+        if (str.Length == 0) return string.Empty;
+        
+        char c = str.FirstOrDefault(c => str.Count(cc => cc.ToString().ToLower().Equals(c.ToString().ToLower())) == 1);
+
+        return !c.Equals('\0') ? c.ToString() : string.Empty;
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine($"{FirstNonRepeatingLetter("")} == string.Empty");
+        Console.WriteLine($"{FirstNonRepeatingLetter("aa")} == string.Empty");
+        
+        Console.WriteLine($"{FirstNonRepeatingLetter("a")} == a");
+        Console.WriteLine($"{FirstNonRepeatingLetter("stress")} == t");
+        Console.WriteLine($"{FirstNonRepeatingLetter("moonmen")} == e");
+    }
+    
+}
