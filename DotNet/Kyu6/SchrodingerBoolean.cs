feacc1a94e7ab84aa542921fb305d2433946c342
yumerov <yumerov.levent@gmail.com>
2025-05-24 23:39:53 +0300

    [kyu6][dotnet] Schrödinger's Boolean

---
+++ DotNet/Kyu6/SchrodingerBoolean.cs
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/5a5f9f80f5dc3f942b002309/train/csharp
+
+namespace DotNet.Kyu6;
+
+public class SchrodingerBoolean
+{
+    private static bool _omnibool = true;
+    public static bool omnibool
+    {
+        get {
+            var result = _omnibool;
+            _omnibool = !_omnibool;
+            return result;
+        }
+    }
+
+    public static void Main() => Console.WriteLine($"{omnibool} -> {omnibool} -> {omnibool} -> {omnibool}");
+}
